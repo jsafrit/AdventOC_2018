de5d1d536e7b8e8dcbc4375297d0de0181dc9b62
John Safrit <jsafrit@users.noreply.github.com>
2018-12-01 14:50:52 -0500

    First pass for Dec 1st

--- puzzle_01a.py
+++ puzzle_01a.py
@@ -1,18 +1,36 @@
-freq = 0
-seen = set()
-seen.add(0)
-dup = None
+"""
+Puzzles for Dec 1st
+"""
 
-while True:
+
+def get_final_freq():
+    freq = 0
+
     with open('input.txt', 'r') as infile:
         for change in infile:
             freq += int(change)
-            if freq not in seen:
-                seen.add(freq)
-            else:
-                print('Duplicate Freq Found: {}'.format(freq))
-                dup = freq
-                break
-    if dup:
-        break
-    print(f'End freq: {freq}')
+    print(f'Final freq: {freq}')
+    return freq
+
+
+def get_first_duplicate_freq():
+    freq = 0
+    seen = set()
+    seen.add(0)
+    dup = None
+
+    while True:
+        with open('input.txt', 'r') as infile:
+            for change in infile:
+                freq += int(change)
+                if freq not in seen:
+                    seen.add(freq)
+                else:
+                    print(f'Duplicate Freq Found: {freq}')
+                    return freq
+        # print(f'End freq: {freq}')
+
+
+if __name__ == '__main__':
+    get_final_freq()
+    get_first_duplicate_freq()
